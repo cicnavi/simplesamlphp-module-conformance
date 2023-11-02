1609d5c83502098bdbe27c3862c7ae61360e11c0
Marko Ivančić <marko.ivancic@srce.hr>
2023-11-02 10:37:36 +0100

    WIP

--- src/Responder/ResponderResolver.php
+++ src/Responder/ResponderResolver.php
@@ -2,12 +2,8 @@
 
 namespace SimpleSAML\Module\conformance\Responder;
 
-use SimpleSAML\Module\conformance\Responder\TestResponder;
-
 class ResponderResolver
 {
-	public const DEFAULT_RESPONDER_CLASS = TestResponder::class;
-
 	public function fromTestId(string $testId, ResponderInterface $responder = null): ?array
 	{
 		$responder = $responder ?? new TestResponder();
