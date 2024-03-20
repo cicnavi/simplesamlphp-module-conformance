d5f4b673e6dbe7892d759297054af6562ac1e533
Marko Ivančić <marko.ivancic@srce.hr>
2024-03-20 14:21:13 +0100

    Note contact email for SP consent

---
+++ migrations/_4_AddContactEmailToSpConsentRequestsTable.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use SimpleSAML\Module\conformance\Database\AbstractDbEntity;
+use SimpleSAML\Module\conformance\Database\MigrationInterface;
+
+class _4_AddContactEmailToSpConsentRequestsTable extends AbstractDbEntity implements MigrationInterface
+{
+    public function run(): void
+    {
+        $this->database->write(<<< EOT
+        ALTER TABLE {$this->getPrefixedTableName()}
+            ADD contact_email VARCHAR(255) NOT NULL AFTER challenge
+        
+EOT
+        );
+    }
+
+    public static function getTableName(): string
+    {
+        return 'sp_consent_requests';
+    }
+}
